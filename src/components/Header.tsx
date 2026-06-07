ccb88439c2f3bf2e2fa1fceae123bb78bea66f04
kangcommit <ahmad.codespace@gmail.com>
2026-06-07 23:00:31 +0700

    chore: update logo and title in Header component to reflect Chef Ollama

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
 import type { JSX } from "react";
-import chefClaudeLogo from "../assets/chef-claude-icon.png";
+import chefOllamaLogo from "../assets/chef-ollama-icon.png";
 
 function Header(): JSX.Element {
 	return (
 		<header className="flex-row h-28 flex gap-3 items-center justify-center shadow-sm">
-			<img src={chefClaudeLogo} alt="Chef Claude" className="w-10.5" />
-			<h1 className="text-[#141413] text-[32px]">Chef Claude</h1>
+			<img src={chefOllamaLogo} alt="Chef Ollama" className="w-10.5" />
+			<h1 className="text-[#141413] text-[32px]">Chef Ollama</h1>
 		</header>
 	);
 }
